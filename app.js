0802fd682559885824feaf28e984a0a18b7f9eaf
kkamara <kelvinkamara@protonmail.com>
2022-11-25 16:29:07 +0000

    Update indentation

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 const { spawn } = require('node:child_process')
 const { log, } = require('node:console')
 const cli = require('cli'), options = cli.parse({
-time: [ 't', 'An access time', 'time', false],                 // -t, --time TIME   An access time
-work: [ 'w', 'What kind of work to do', 'string', 'sleep' ]  //     --work STRING What kind of work to do
+  time: [ 't', 'An access time', 'time', false],                 // -t, --time TIME   An access time
+  work: [ 'w', 'What kind of work to do', 'string', 'sleep' ]  //     --work STRING What kind of work to do
 })
 
 const run = () => {
@@ -13,19 +13,19 @@ const run = () => {
   const ls = spawn("ls", ["-la"])
 
   ls.stdout.on("data", data => {
-      log(`stdout: ${data}`)
+    log(`stdout: ${data}`)
   })
 
   ls.stderr.on("data", data => {
-      log(`stderr: ${data}`)
+    log(`stderr: ${data}`)
   })
 
   ls.on('error', (error) => {
-      log(`error: ${error.message}`)
+    log(`error: ${error.message}`)
   })
 
   ls.on("close", code => {
-      log(`child process exited with code ${code}`)
+    log(`child process exited with code ${code}`)
   })
 }
 
